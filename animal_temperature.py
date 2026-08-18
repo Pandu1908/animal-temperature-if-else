c9fbdce5a452ecda9c737dc4a0de74ec47444739
Pandu1908 <252t604007@ashokacollege.in>
2026-08-18 17:14:57 +0530

    animal_temperature.py
    This Python program demonstrates temperature checking using an if-else conditional statement. It accepts an animal's temperature using input() and converts it to a floating-point value with float(). If the temperature is above 39.5, the program displays a high-temperature warning and advises checking the animal. Otherwise, it displays that the temperature is normal.

---
+++ animal_temperature.py
@@ -0,0 +1,7 @@
+temperature = float(input("Enter animal temperature: "))
+
+if temperature > 39.5:
+    print("⚠️ Temperature is high.")
+    print("🐄 Check the animal.")
+else:
+    print("✅ Temperature is normal.")
